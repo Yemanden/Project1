9725b4b65211c8e8b1e27136dd667e8fbd5f844b
Yemanden <V1997V@rambler.ru>
2017-07-05 09:17:11 +0300

    init

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
-// main.go
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
+
+type Vertex struct {
+	x int
+	y int
+}
+
+var s = map[string]Vertex{
+	"aaa": {9, 9},
+	"hhh": {5, 6},
+	"a":   {0, 0},
+	"ggg": {4, 4},
+}
 
 func main() {
-	fmt.Println("Hello World!")
-	fmt.Println(time.Now())
+	fmt.Println(s)
+
 }
